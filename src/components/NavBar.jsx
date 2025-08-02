208969fa186bb8907ffa3512229d74c1ee0d1e17
Krish Gupta <179193922+blobmister@users.noreply.github.com>
2025-08-02 13:49:39 +1000

    Made nav-bar responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import './NavBar.css';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import downarrow from '../assets/arrow-down.png';
+import hamburger from '../assets/Hamburger_icon.svg';
 
 
 function Dropdown({ isActive, items}) {
@@ -23,20 +24,34 @@ function Dropdown({ isActive, items}) {
 export default function NavBar() {
     let [showAboutDrop, toggleAboutDrop] = useState(false);
     let [showServicesDrop, toggleServicesDrop] = useState(false);
+    let [showList, toggleShowList] = useState(false);
+    let [aboutClick, toggleAboutClick] = useState(false);
+    let [serviceClick, toggleServiceClick] = useState(false);
+
     return (
         <nav className='navbar'>
-            <Link to='/' className='logo'>
-                <img alt='Clever Chap Logo'/>
-            </Link>
+            <div className="row1">
+                <Link to='/' className='logo'>
+                    <img alt='Clever Chap Logo'/>
+                </Link>
+                <button onClick={() => toggleShowList(prev => !prev)} className='hamburger'> <img src={hamburger} alt='hamburger icon'/></button>
+            </div>
 
-            <ul className="nav-list">
+            <ul className={showList ? ("show nav-list") : ("nav-list")}>
                 <li><Link to='/' className='nav-link'>Home</Link></li>
                 <li><Link to='/get-quote' className='nav-link'>Quote Price</Link></li>
 
-                <li onMouseEnter={() => toggleServicesDrop(true)} onMouseLeave={() => toggleServicesDrop(false)}>
+                <li 
+                    onMouseEnter={() => toggleServicesDrop(true)} 
+                    onMouseLeave={() => toggleServicesDrop(false)} 
+                    onClick={() => {
+                        toggleServiceClick(prev => !prev);
+                        toggleAboutClick(false);
+                    }}
+                >
                     <div className='nav-link'>Services<img src={downarrow} alt='dropdown button'/></div>
                     <Dropdown 
-                        isActive={showServicesDrop}
+                        isActive={(showServicesDrop || serviceClick)}
                         items= {[
                             {name: 'Commercial Cleaning', to: '/commercial-cleaning'},
                             {name: 'Residential Cleaning', to: '/residential-cleaning'},
@@ -44,10 +59,19 @@ export default function NavBar() {
                     />
                 </li>
 
-                <li onMouseEnter={() => toggleAboutDrop(true)} onMouseLeave={() => toggleAboutDrop(false)}>
+                <li onMouseEnter={() => 
+                    toggleAboutDrop(true)} 
+                    onMouseLeave={() => toggleAboutDrop(false)}
+                    onClick={() => {
+                        toggleAboutClick(prev => !prev)
+                        toggleServiceClick(false);
+                    }
+
+                    }
+                >
                     <div className='nav-link'>About Us<img src={downarrow} alt='dropdown button'/></div>
                     <Dropdown 
-                        isActive={showAboutDrop}
+                        isActive={(showAboutDrop || aboutClick)}
                         items= {[
                             {name: 'Who We Are', to: '/about'},
                             {name: 'Contact', to: '/contact'},
